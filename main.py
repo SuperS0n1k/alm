e9e1034889d7cc5ba52fdd9317f723846def4bcb
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-08-25 13:24:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import scratchattach as scratch3
 
 session = scratch3.login("Arbal1", "Arbals")
 print(session.session_id)
+iteratons = 1
+while True:
+  user = session.connect_user("griffpatch")
+  user.post_comment("Become A Follower. Be Part of The Movement. This message has been delivered " + str(iteratons) + "times")
+  iteratons = iteratons + 1
+  time.sleep(1)
