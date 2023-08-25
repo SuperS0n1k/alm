597883f50af9e575ed4e8e5f1208bc18e6daf12d
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-08-25 16:07:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ print(session.session_id)
 iteratons = 1
 while True:
   user = session.connect_user("griffpatch")
-  user.post_comment("Become A Follower. Be Part of The Movement. This message has been delivered " + str(iteratons) + "times")
+  user.post_comment("Become A Follower. Be Part of The Movement. This message has been delivered " + str(iteratons) + " times")
   iteratons = iteratons + 1
   time.sleep(1)
