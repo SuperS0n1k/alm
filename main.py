80514dc29aad7722d738c4a0ea765ca24b0ab2b9
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-08-25 16:29:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import scratchattach as scratch3
 import time
-session = scratch3.login("Arbal1", "Arbals")
+session = scratch3.login("msgBOT", "Arbals")
 print(session.session_id)
-iteratons = 1
 while True:
-  user = session.connect_user("griffpatch")
-  user.post_comment("This is an unofficial Scratch Test. This message has been delivered " + str(iteratons) + " times. ⏶ Please do not report this comment ⏶")
-  iteratons = iteratons + 1
+  session.connect_user("CraZCoder360")
+  user.follow()
   time.sleep(1)
+  user.unfollow()
+  
