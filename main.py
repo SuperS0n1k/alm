3094dd083b7f819f0ce5377ecd34299e69a0a44a
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-08-25 16:38:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import scratchattach as scratch3
 import time
+from random import randint
 session = scratch3.login("msgBOT", "Arbals")
 print(session.session_id)
 while True:
   user = session.connect_user("CraZCoder360")
-  user.follow()
+  user.post_comment(content=randint(0,999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999))
   time.sleep(1)
-  user.unfollow()
-  
