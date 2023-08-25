c2c6e4478bf096993c2d02540fa228e3042cc408
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-08-25 16:40:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from random import randint
 session = scratch3.login("msgBOT", "Arbals")
 print(session.session_id)
 while True:
-  user = session.connect_user("CraZCoder360")
+  user = session.connect_user("griffpatch")
   user.post_comment(content=randint(0,999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999))
   time.sleep(1)
